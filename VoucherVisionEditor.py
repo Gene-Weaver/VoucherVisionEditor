b46e200fa6435a04d6977e2dc66f5ab4559c4b94
Gene-Weaver <willwe@umich.edu>
2023-06-16 03:24:05 -0400

    more configurations, new cropped image format, wider table view

--- VoucherVisionEditor.py
+++ VoucherVisionEditor.py
@@ -139,11 +139,27 @@ if st.session_state.data is None:
 
 if st.session_state.data is not None:
     # Define the four columns
-    c1, c2, c3, c4 = st.columns(4)
-    view_option = c1.selectbox("Choose a View", ["Form View", "Data Editor"])
-    group_option = c2.selectbox("Choose a Category", list(grouping.keys()) + ["ALL"])
+    c1, c3, c4, c5 = st.columns([4,2,1,1])
+    # group_option = c1.selectbox("Choose a Category", list(grouping.keys()) + ["ALL"])
+    group_options = list(grouping.keys()) + ["ALL"]
+    group_option = st.session_state.get("group_option", group_options[0])
 
-    form_col, json_col, image_col = st.columns([1, 1, 2])  
+    group_option_cols = c1.columns(len(group_options))
+
+    for i, option in enumerate(group_options):
+        if group_option_cols[i].button(option):
+            st.session_state["group_option"] = option
+            group_option = option
+
+    image_option = c3.selectbox('Choose an Image', ['Cropped', 'Original'])
+    view_option = c4.selectbox("Choose a View", ["Form View", "Data Editor"])
+    img_loc = c5.selectbox("Image Position", ["Right", "Middle"])
+
+    if img_loc == 'Middle':
+        form_col, image_col, json_col  = st.columns([1, 2, 1])  
+    elif img_loc == 'Right':
+        form_col, json_col, image_col  = st.columns([1, 1, 2])  
+
 
 
     if view_option == "Form View":
@@ -187,7 +203,26 @@ if st.session_state.data is not None:
         # Create two columns for the data editor and the image
         # data_editor_col, image_col = st.columns([1, 1])  # Divide the space equally between the two columns
 
-        with form_col:
+        # with form_col:
+        #     edited_data = st.data_editor(st.session_state.data)
+
+        #     if st.button("Save Edits"):
+        #         # Save the edited data back to the session state data
+        #         st.session_state.data = edited_data
+        #         save_data_editor()
+
+        #     # Slider or number input to select the row
+        #     slider_value = st.slider("Select a row to display its image", min_value=st.session_state.data.index[0], max_value=st.session_state.data.index[-1], value=st.session_state.row_to_edit)
+            
+        #     # Only update the row_to_edit if slider value changes
+        #     if slider_value != st.session_state.row_to_edit:
+        #         st.session_state.row_to_edit = slider_value
+
+        #     # Display the current row
+        #     n_rows = len(st.session_state.data)-1
+        #     st.write(f"**Editing row {st.session_state.row_to_edit} / {n_rows}**")
+        # If the view option is "Data Editor", create a new full-width container for the editor
+        with st.container():
             edited_data = st.data_editor(st.session_state.data)
 
             if st.button("Save Edits"):
@@ -196,16 +231,24 @@ if st.session_state.data is not None:
                 save_data_editor()
 
             # Slider or number input to select the row
-            slider_value = st.slider("Select a row to display its image", min_value=st.session_state.data.index[0], max_value=st.session_state.data.index[-1], value=st.session_state.row_to_edit)
-            
-            # Only update the row_to_edit if slider value changes
-            if slider_value != st.session_state.row_to_edit:
-                st.session_state.row_to_edit = slider_value
+            # Only display the slider if there are 2 or more rows
+            if len(st.session_state.data) >= 2:
+                slider_value = st.slider("Select a row to display its image", min_value=st.session_state.data.index[0], max_value=st.session_state.data.index[-1], value=st.session_state.row_to_edit)
+
+                # Only update the row_to_edit if slider value changes
+                if slider_value != st.session_state.row_to_edit:
+                    st.session_state.row_to_edit = slider_value
 
             # Display the current row
             n_rows = len(st.session_state.data)-1
             st.write(f"**Editing row {st.session_state.row_to_edit} / {n_rows}**")
 
+            
+        # Create separate columns for image and JSON as they are below the editor in this case
+        if img_loc == 'Middle':
+            image_col, json_col  = st.columns([1, 1])  
+        elif img_loc == 'Right':
+            json_col, image_col  = st.columns([1, 1]) 
 
 
     # check if the row_to_edit has changed
@@ -229,32 +272,60 @@ if st.session_state.data is not None:
         if 'json_dict' in st.session_state:
             json_dict = st.session_state['json_dict']
 
-            print(group_option)
-            for key, value in json_dict.items():
+            for main_key, main_value in json_dict.items():
                 if group_option == 'ALL':
-                    color = color_map_json.get(key, "black")  # Default to black if key is not in color_map
-                    st.markdown(f"<h4 style='color: {color};'>{key}</h4>", unsafe_allow_html=True)
-                    for val in value:
-                        st.markdown(f"<p style='font-family:times new roman; font-size:20px;'>{val}</p>", unsafe_allow_html=True)  # Use HTML and CSS to style the text
-                elif (key == group_option) or (key == 'MISCELLANEOUS'):
-                    color = color_map_json.get(key, "black")  # Default to black if key is not in color_map
-                    st.markdown(f"<h4 style='color: {color};'>{key}</h4>", unsafe_allow_html=True)
-                    for val in value:
-                        st.markdown(f"<p style='font-family:times new roman; font-size:20px;'>{val}</p>", unsafe_allow_html=True)  # Use HTML and CSS to style the text
+                    color = color_map_json.get(main_key, "black")  # Default to black if key is not in color_map
+                    st.markdown(f"<h4 style='color: {color};'>{main_key}</h4><br>", unsafe_allow_html=True)
+                    if isinstance(main_value, dict):
+                        for sub_key, sub_value in main_value.items():
+                            if sub_value:
+                                st.markdown(f"<b style='font-size:20px;'>{sub_key}: <br></b> {sub_value}<br>", unsafe_allow_html=True)
+                elif (main_key == group_option) or (main_key == 'MISCELLANEOUS'):
+                    color = color_map_json.get(main_key, "black")  # Default to black if key is not in color_map
+                    st.markdown(f"<h4 style='color: {color};'>{main_key}</h4><br>", unsafe_allow_html=True)
+                    if isinstance(main_value, dict):
+                        for sub_key, sub_value in main_value.items():
+                            if sub_value:
+                                st.markdown(f"<b style='font-size:20px;'>{sub_key}: <br></b> {sub_value}<br>", unsafe_allow_html=True)
 
 
+
+    # # Only load image if row has changed
+    # with image_col:
+    #     if st.session_state['last_row_to_edit'] != st.session_state.row_to_edit:
+    #         st.session_state['image_path'] = st.session_state.data.loc[st.session_state.row_to_edit, "path_to_original"]
+    #         st.session_state['image_path'] = st.session_state.data.loc[st.session_state.row_to_edit, "path_to_crop"]
+    #         if pd.notnull(st.session_state['image_path']):
+    #             print('LOADING IMAGE')
+    #             st.session_state['image'] = Image.open(st.session_state['image_path'])
+                
+    #     # Display image
+    #     if 'image' in st.session_state and 'image_path' in st.session_state:
+    #         image = st.session_state['image']
+    #         st.image(image, caption=st.session_state['image_path'])
     # Only load image if row has changed
     with image_col:
-        if st.session_state['last_row_to_edit'] != st.session_state.row_to_edit:
-            st.session_state['image_path'] = st.session_state.data.loc[st.session_state.row_to_edit, "path_to_crop"]
+
+        # Add a selectbox for the image selection
+
+        if st.session_state['last_row_to_edit'] != st.session_state.row_to_edit or 'last_image_option' not in st.session_state or st.session_state['last_image_option'] != image_option:
+            if image_option == 'Original':
+                st.session_state['image_path'] = st.session_state.data.loc[st.session_state.row_to_edit, "path_to_original"]
+            elif image_option == 'Cropped':
+                st.session_state['image_path'] = st.session_state.data.loc[st.session_state.row_to_edit, "path_to_crop"]
+
             if pd.notnull(st.session_state['image_path']):
                 print('LOADING IMAGE')
                 st.session_state['image'] = Image.open(st.session_state['image_path'])
                 
+            # Remember the selected image option
+            st.session_state['last_image_option'] = image_option
+
         # Display image
         if 'image' in st.session_state and 'image_path' in st.session_state:
             image = st.session_state['image']
             st.image(image, caption=st.session_state['image_path'])
+
 
 
     # update the 'last_row_to_edit' in the session state to the current 'row_to_edit'
