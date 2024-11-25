e013e4786813d999236e49400da9130b10dff77c
Will <william36177@gmail.com>
2024-11-25 13:07:23 -0500

    auto fix missing or old packages

--- run.py
+++ run.py
@@ -156,6 +156,9 @@ if __name__ == '__main__':
         print(f"Could not update VVE using git pull.")
         print(f"Make sure that 'Git' is installed and can be accessed by this user account.")
 
+    # Update again in case the pull introduced a new package
+    check_and_fix_requirements(resolve_path(os.path.join(os.path.dirname(__file__),'requirements.txt')))
+
     try:
         free_port = find_available_port(start_port, end_port)
         sys.argv = [
