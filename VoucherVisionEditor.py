3f8f6f9d79e55c38bfd97dd03f71142d9726a8ae
Will <william36177@gmail.com>
2024-12-19 11:19:02 -0500

    update via zip only fix git import

--- VoucherVisionEditor.py
+++ VoucherVisionEditor.py
@@ -52,7 +52,7 @@ st.set_page_config(layout="wide",
                    menu_items={"Report a Bug": "https://forms.gle/kP8imC9JQTPLrXgg8","About":"VoucherVision was created and is maintained by William Weaver, University of Michigan. Please see doi:10.1002/ajb2.16256 for more information."},
                    initial_sidebar_state="expanded",)
 
-LAST_UPDATED = "Dec 17, 2024"
+LAST_UPDATED = "Dec 19, 2024"
 ###########################################################################################################################
 ######## If true, this will let you host the project remotely
 ######## Otherwise all project info will be hosted inside VoucherVisionEditor/ on the local machine
